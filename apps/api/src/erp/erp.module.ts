59f79b881a00b84035e44263cc20d61b422e0f7b
mercadozendobrasil-arch <mercadozendobrasil@gmail.com>
2026-04-27 12:59:24 -0300

    feat(api): register order manual actions module providers

--- apps/api/src/erp/erp.module.ts
+++ apps/api/src/erp/erp.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { ErpJobsController } from './jobs/erp-jobs.controller';
 import { ErpJobsService } from './jobs/erp-jobs.service';
+import { ErpOrderActionsController } from './orders/erp-order-actions.controller';
+import { ErpOrderActionsService } from './orders/erp-order-actions.service';
 import { ErpOrdersController } from './orders/erp-orders.controller';
 import { ErpOrdersService } from './orders/erp-orders.service';
 
 @Module({
-  controllers: [ErpJobsController, ErpOrdersController],
-  providers: [ErpJobsService, ErpOrdersService],
+  controllers: [ErpJobsController, ErpOrderActionsController, ErpOrdersController],
+  providers: [ErpJobsService, ErpOrderActionsService, ErpOrdersService],
 })
 export class ErpModule {}
