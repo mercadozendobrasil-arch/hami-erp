9049cb88d35812580f470b0f86d1442d8364c4ba
mercadozendobrasil-arch <mercadozendobrasil@gmail.com>
2026-05-03 12:23:23 -0300

    fix(shopee): use sandbox auth host and configured redirect

--- apps/api/src/common/shopee.constants.ts
+++ apps/api/src/common/shopee.constants.ts
@@ -4,7 +4,7 @@ export const SHOPEE_AUTH_REFRESH_JOB = 'refresh-access-token';
 export const SHOPEE_DEFAULT_API_BASE_URL = 'https://partner.shopeemobile.com';
 export const DEFAULT_SHOPEE_BASE_URL = SHOPEE_DEFAULT_API_BASE_URL;
 export const SHOPEE_SANDBOX_BASE_URL =
-  'https://partner.test-stable.shopeemobile.com';
+  'https://openplatform.sandbox.test-stable.shopee.sg';
 
 export const SHOPEE_ACCESS_TOKEN_TTL_SECONDS = 4 * 60 * 60;
 export const SHOPEE_REFRESH_TOKEN_TTL_SECONDS = 30 * 24 * 60 * 60;
